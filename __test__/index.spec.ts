9e92e869dcf168cc9be2867367fa7059fbde9bf8
ToddyTheNoobDud <jmiguel.franchini@gmail.com>
2026-03-23 14:01:44 -0300

    feat: initial udpqueue implementation
    - Add UdpQueueManager with fixed 20ms send cadence
    - Support Windows/Linux with platform-specific timing
    - Migrate from yarn to pnpm

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,27 @@
 import test from 'ava'
+import { createRequire } from 'module'
 
-import { plus100 } from '../index'
+const require = createRequire(import.meta.url)
+const { UdpQueueManager } = require('../index.js')
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+test('UdpQueueManager can be created', (t) => {
+  const qm = new UdpQueueManager(400)
+  t.true(qm instanceof UdpQueueManager)
+  qm.deleteQueue(0)
+})
+
+test('UdpQueueManager createQueue returns valid key', (t) => {
+  const qm = new UdpQueueManager(400)
+  const key = qm.createQueue('127.0.0.1', 9001, null)
+  t.is(typeof key, 'number')
+  t.true(key >= 0)
+  qm.deleteQueue(key)
+})
+
+test('UdpQueueManager can push and delete queue', (t) => {
+  const qm = new UdpQueueManager(400)
+  const key = qm.createQueue('127.0.0.1', 9001, null)
+  const pushed = qm.pushPacket(key, new Uint8Array([1, 2, 3]))
+  t.true(pushed)
+  qm.deleteQueue(key)
 })
--- browser.js
+++ browser.js
@@ -1,1 +1,1 @@
-export * from '@napi-rs/package-template-wasm32-wasi'
+export * from '@toddynnn/udpqueue-wasm32-wasi'
